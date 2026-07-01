f2e152502d922d9dd1e84e44f5786923089c3423
LAPTOP-TOLAO080\ALI <alibinambo26@gmail.com>
2026-07-01 10:21:47 +0800

    add configured Trusted Proxies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,5 +16,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         User::observe(UserObserver::class);
+
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
